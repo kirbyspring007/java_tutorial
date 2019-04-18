bcfe51991e6f1d6933c2359ed629e219b2c02169
JEON YONGSUK <49046888+kirbyspring007@users.noreply.github.com>
2019-04-18 13:25:01 +0900

    loop

---
+++ java_tutorial/src/org/java_tutorial/loop/WhileDemo.java
@@ -0,0 +1,16 @@
+package org.java_tutorial.loop;
+
+public class WhileDemo {
+
+	public static void main(String[] args) {
+		int i = 0;
+		while(i<10){         
+		    System.out.println("Coding Everybody"+i);
+		    
+		    i++;
+		}
+		   
+
+	}
+
+}
