d44f759c50388d01ca156bf09b0005afdc6467ed
JEON YONGSUK <49046888+kirbyspring007@users.noreply.github.com>
2019-04-27 09:20:00 +0900

    2019/04/27

--- java_tutorial/src/org/java_tutorial/exception/CalculatorDemo.java
+++ java_tutorial/src/org/java_tutorial/exception/CalculatorDemo.java
@@ -1,11 +1,14 @@
 package org.java_tutorial.exception;
 class Calculator{
     int left, right;
-    public void setOprands(int left, int right){
+    public void setOprands(int left, int right){        
         this.left = left;
         this.right = right;
     }
     public void divide(){
+        if(this.right == 0){
+            throw new ArithmeticException("0으로 나누는 것은 허용되지 않습니다.");
+        }
         try {
             System.out.print("계산결과는 ");
             System.out.print(this.left/this.right);
