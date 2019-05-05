098d8556a1950c39c14dc5b2599a4253f651375e
JEON YONGSUK <49046888+kirbyspring007@users.noreply.github.com>
2019-05-05 14:00:41 +0900

    2019/05/05

--- java_tutorial/src/org/java_tutorial/constant2/ConstantDemo.java
+++ java_tutorial/src/org/java_tutorial/constant2/ConstantDemo.java
@@ -1,21 +1,26 @@
 package org.java_tutorial.constant2;
 
+enum Fruit{
+   APPLE("red"), PEACH("pink"), BANANA("yellow");
+   private String color;
+   Fruit(String color){
+       System.out.println("Call Constructor "+this);
+       this.color = color;
+   }
+   String getColor(){
+       return this.color;
+   }
+}
+
+enum Company{
+   GOOGLE, APPLE, ORACLE;
+}
+
 public class ConstantDemo {
-   private final static int APPLE = 1;
-   private final static int PEACH = 2;
-   private final static int BANANA = 3;
+    
    public static void main(String[] args) {
-       int type = APPLE;
-       switch(type){
-           case APPLE:
-               System.out.println(57+" kcal");
-               break;
-           case PEACH:
-               System.out.println(34+" kcal");
-               break;
-           case BANANA:
-               System.out.println(93+" kcal");
-               break;
+       for(Fruit f : Fruit.values()){
+           System.out.println(f+", "+f.getColor());
        }
    }
 }
