61fe8e1dca25446b7774c57bb2092f2a9a71f3d2
JEON YONGSUK <49046888+kirbyspring007@users.noreply.github.com>
2019-05-07 21:27:45 +0900

    2019/05/07

--- java_tutorial/src/org/java_tutorial/reference/ReferenceDemo1.java
+++ java_tutorial/src/org/java_tutorial/reference/ReferenceDemo1.java
@@ -1,16 +1,29 @@
 package org.java_tutorial.reference;
-
+class A{
+    public int id;
+    A(int id){
+        this.id = id;
+    }
+}
 public class ReferenceDemo1 {
-
-   public static void runValue(){
-       int a = 1;
-       int b = a;
-       b = 2;
-       System.out.println("runValue, "+a); 
-   }
-
-   public static void main(String[] args) {
-       runValue();
-   }
-
+ 
+    public static void runValue(){
+        int a = 1;
+        int b = a;
+        b = 2;
+        System.out.println("runValue, "+a); 
+    }
+     
+    public static void runReference(){
+        A a = new A(1);
+        A b = a;
+        b.id = 2;
+        System.out.println("runReference, "+a.id);      
+    }
+ 
+    public static void main(String[] args) {
+        runValue();
+        runReference();
+    }
+ 
 }
